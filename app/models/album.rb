4355f7c1ca7202e88516f8a091b80901e6fc57d8
prabhakar <undurthi.prabhakar@gmail.com>
2014-03-14 15:54:19 +0530

    Implemented the feature controller

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,11 @@
 class Album < ActiveRecord::Base
+
+
+	belongs_to :artist
+	has_and_belongs_to_many :features
   
   validates_presence_of :title, :artist
   validates_length_of :title, :minimum => 5
-  validates_uniqueness_of :artist
+  #validates_uniqueness_of :artist
   
 end

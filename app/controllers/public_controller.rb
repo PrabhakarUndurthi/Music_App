7b166d52f5dae498e1238b5a08d53c111b26781c
prabhakar <undurthi.prabhakar@gmail.com>
2014-03-15 18:07:03 +0530

    Added credit card payments process.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,8 @@
 class PublicController < ApplicationController
 
-	before_filter :find_or_create_cart, :only => [:add_to_cart, :show_cart,:empty_cart]
+	before_filter :find_or_create_cart, :only => [:add_to_cart, :show_cart,:empty_cart,:remove_from_cart]
 
+     before_filter :find_or_create_cart, :except => [:list]
     layout 'standard'
 
 	def list
@@ -25,6 +26,13 @@ class PublicController < ApplicationController
 		#@Cart = find_or_create_cart
 	end
 
+	def remove_from_cart
+    album = Album.find(params[:id])
+    @cart.remove_album(album)
+    flash[:notice] = 'One item was removed from your cart.'
+    redirect_to(:action => 'show_cart')
+  end
+
 	def empty_cart
 		#it removes all the items from the cart
 		# and redirect the user to the list to do more shopping.
@@ -32,6 +40,32 @@ class PublicController < ApplicationController
 		flash[:notice] = 'Your cart is empty.'
 		redirect_to(:action => 'list')
 	end
+
+	def checkout
+    @customer = Customer.new
+  end
+  
+  def save_order
+    @customer = Customer.new(params[:customer])
+    credit_card_number = params[:credit_card]
+    @order = Order.new
+    @order.line_items << @cart.items
+    @customer.orders << @order
+    if @customer.save
+      #try to process payment
+      #if payment fails, send user to a fix-it page
+      #if payment succeeds
+      @cart.empty_all_items
+      flash[:notice] = 'Your purchase was successfully completed.'
+      redirect_to(:action => 'show_receipt', :id => @order.id)
+    else
+      render(:action => 'checkout')
+    end
+  end
+
+  def show_receipt
+    @order = Order.find(params[:id])
+  end
 
 
 	private #..................
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,32 +1,54 @@
-class Cart < ActiveRecord::Base
 
-	attr_reader :items
-	attr_reader :total_price
+class Cart
+  
+  attr_reader :items
+  attr_reader :total_price
+  
+  def initialize
+    empty_all_items
+  end
 
-	def initialize
-        empty_all_items
-	end
+  #if the user wants to remove all the items from the shopping cart.
+  
+  def empty_all_items
+    @items = []
+    @total_price = 0.0
+  end
 
-	def empty_all_items
-		@items = []
-		@total_price 0.0
-	end
-
-
-
-    # when the user select an album then, we are ready to add that album to the shopping cart
+  #when the user select an album then, we are ready to add that album to the shopping cart
     # and display the pirce for that album
 
+  
+  def add_album( album )
+    existing_item = @items.find {|item| item.album_id == album.id}
+    if existing_item
+      existing_item.quantity += 1
+    else
+      @items << LineItem.new_based_on(album)
+    end
+    @total_price += album.price
+  end
 
-	def add_album(album)
-		existing_item = @items.find{|item| item.album_id ==album.id}
-	# if the item is already exists? then increase the quantity
-	if existing_item
-	  existing_item.quantity += 1
-	else
-		@items << LineItem.new_based_on(album)
-	  end
-	  @total_price += album.price
-   end
-
+  #if the user wants to remove or delete an item
+  #it would let the user to make that happen.
+  
+  def remove_album( album )
+    existing_item = @items.find {|item| item.album_id == album.id}
+    if existing_item && existing_item.quantity > 1
+      existing_item.quantity -= 1
+    else
+      @items.delete(existing_item)
+    end
+    @total_price -= album.price
+  end
+  
 end
+
+
+
+
+
+
+    
+
+	

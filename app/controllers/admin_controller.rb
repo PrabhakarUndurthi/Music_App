5a89f764b5430577c16d858aa01644729110b1e9
prabhakar <undurthi.prabhakar@gmail.com>
2014-03-15 18:53:56 +0530

    Final commit.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,31 +4,38 @@ class AdminController < ApplicationController
 
   before_filter :authorize_access, :except => [:login, :send_login]
 
-  def index
-    menu
-    render(:action => 'menu')
-  end
-  
-  def menu
-    # no instance variables declared, just a page of links
-  end
-  
-  def login
-    @user = User.new
-  end
-  
-  def send_login
-    @user = User.new(params[:user])
-    logged_in_user = @user.try_to_login
-    if logged_in_user
-      session[:user_id] = logged_in_user.id
-      flash[:notice] = "You are now logged in."
-      redirect_to(:action => 'menu')
-    else
-      flash[:notice] = "Username/password combination incorrect. Please make sure your caps lock key is off and try again."
-      redirect_to(:action => 'login')
+    def index
+      menu
+      render(:action => 'menu')
     end
-  end
+  
+    def menu
+      # no instance variables declared, just a page of links
+    end
+    
+    def login
+      @user = User.new
+    end
+    
+
+  # when the user logged in to the site , it checks whether the credentials are not matched 
+  # or not, if matched it redirect the user to the login page and display the menu.
+    def send_login
+      @user = User.new(params[:user])
+      logged_in_user = @user.try_to_login
+      if logged_in_user
+        session[:user_id] = logged_in_user.id
+        flash[:notice] = "You are now logged in."
+        redirect_to(:action => 'menu')
+      else
+        flash[:notice] = "Username/password combination incorrect. Please make sure your caps lock key is off and try again."
+        redirect_to(:action => 'login')
+      end
+    end
+
+
+    # if the user logged out from the site, it redirect the user to the default login page.
+    
   
   def logout
     session[:user_id] = nil
--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,92 +1,102 @@
-class AlbumsController < ApplicationController
-  def index
-    list
-    render :action => 'list'
-  end
-
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
-
-  def list
-    @album_pages, @albums = paginate :albums, :per_page => 10
-  end
-
-  def show
-    @album = Album.find(params[:id])
-  end
-
-  def new
-    @artists = Artist.find(:all)
-    @features = Feature.find(:all)
-    @album = Album.new
-  end
-
-  def create
-    @album = Album.new(params[:album])
-    artist = Artist.find_by_id(params[:artist_id])
-    @album.artist = artist
-    @features = Feature.find(:all)
-    checked_features = check_features_using(params[:feature_list])
-    uncheck_missing_features(@features, checked_features)
-    if @album.save
-      flash[:notice] = 'Album was successfully created.'
-      redirect_to :action => 'list'
-    else
-      @artists = Artist.find(:all)
-      render :action => 'new'
+  class AlbumsController < ApplicationController
+    def index
+      list
+      render :action => 'list'
     end
-  end
 
-  def edit
-    @artists = Artist.find(:all)
-    @features = Feature.find(:all)
-    @album = Album.find(params[:id])
-  end
+    # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+    verify :method => :post, :only => [ :destroy, :create, :update ],
+           :redirect_to => { :action => :list }
 
-  def update
-    @album = Album.find(params[:id])
-    artist = Artist.find_by_id(params[:artist_id])
-    @album.artist = artist
-    @features = Feature.find(:all)
-    checked_features = check_features_using(params[:feature_list])
-    uncheck_missing_features(@features, checked_features)
-    if @album.update_attributes(params[:album])
-      flash[:notice] = 'Album was successfully updated.'
-      redirect_to :action => 'show', :id => @album
-    else
-      @artists = Artist.find(:all)
-      render :action => 'edit'
+    def list
+      @album_pages, @albums = paginate :albums, :per_page => 10
     end
-  end
 
-  def destroy
-    Album.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
-  
-  private #-------------------------
-  
-  def check_features_using( feature_id_array )
-    checked_features = []
-    checked_params = feature_id_array || []
-    for check_box_id in checked_params
-      feature = Feature.find(check_box_id)
-      if not @album.features.include?(feature)
-        @album.features << feature
+    def show
+      @album = Album.find(params[:id])
+    end
+
+    def new
+      @artists = Artist.find(:all)
+      @features = Feature.find(:all)
+      @album = Album.new
+    end
+
+    # creating a new album collection
+
+    def create
+      @album = Album.new(params[:album])
+      artist = Artist.find_by_id(params[:artist_id])
+      @album.artist = artist
+      @features = Feature.find(:all)
+      checked_features = check_features_using(params[:feature_list])
+      uncheck_missing_features(@features, checked_features)
+      if @album.save
+        flash[:notice] = 'Album was successfully created.'
+        redirect_to :action => 'list'
+      else
+        @artists = Artist.find(:all)
+        render :action => 'new'
       end
-      checked_features << feature
     end
-    return checked_features
-  end
-  
-  def uncheck_missing_features( features, checked_features)
-    missing_features = features - checked_features
-    for feature in missing_features
-      if @album.features.include?(feature)
-         @album.features.delete(feature)
-       end
+
+    # allowing the user to edit the existing album
+    # and some of its feeatures.
+
+    def edit
+      @artists = Artist.find(:all)
+      @features = Feature.find(:all)
+      @album = Album.find(params[:id])
     end
+
+    #once it is done, user can go back and update the info on the album collection
+
+
+    def update
+      @album = Album.find(params[:id])
+      artist = Artist.find_by_id(params[:artist_id])
+      @album.artist = artist
+      @features = Feature.find(:all)
+      checked_features = check_features_using(params[:feature_list])
+      uncheck_missing_features(@features, checked_features)
+      if @album.update_attributes(params[:album])
+        flash[:notice] = 'Album was successfully updated.'
+        redirect_to :action => 'show', :id => @album
+      else
+        @artists = Artist.find(:all)
+        render :action => 'edit'
+      end
+    end
+
+    # Users can delete the albums at any time.
+
+    def destroy
+      Album.find(params[:id]).destroy
+      redirect_to :action => 'list'
+    end
+    
+    private #-------------------------
+    
+    def check_features_using( feature_id_array )
+      checked_features = []
+      checked_params = feature_id_array || []
+      for check_box_id in checked_params
+        feature = Feature.find(check_box_id)
+        if not @album.features.include?(feature)
+          @album.features << feature
+        end
+        checked_features << feature
+      end
+      return checked_features
+    end
+    
+    def uncheck_missing_features( features, checked_features)
+      missing_features = features - checked_features
+      for feature in missing_features
+        if @album.features.include?(feature)
+           @album.features.delete(feature)
+         end
+      end
+    end
+    
   end
-  
-end
